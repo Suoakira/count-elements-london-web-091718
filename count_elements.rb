265c896a735158836cc7e5bd2e998e67c120026b
github username <github email address>
2018-09-20 05:33:11 +0100

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -3,3 +3,20 @@ def count_elements(array)
   array.each { |word| new_hash[word] += 1 }
   return new_hash
 end
+
+require 'pry'
+class String
+  def sentence
+    self.end_with(".") ? true : false
+  end
+  def question?
+    self.end_with("?") ? true : false
+  end
+  def explanation
+    self.end_with ? true : false
+  end
+  def count_sentences
+    split_strings = self.split(/[!?.]+/)
+    split_strings.size
+  end
+end
